7292a98c7689dd1e23676fdcda9c6e68a1ffccc9
xschen <xs0040@gmail.com>
2018-02-12 11:54:20 +0800

    update credential for valid testing

--- src/test/java/com/github/chen0040/androidmagentoclient/Mediator.java
+++ src/test/java/com/github/chen0040/androidmagentoclient/Mediator.java
@@ -6,11 +6,11 @@ package com.github.chen0040.androidmagentoclient;
  */
 public class Mediator {
 
-   public static final String url = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
-   public static final String customerUsername = "xs0040@gmail.com";
-   public static final String customerPassword = "chen@0040";
-   public static final String adminUsername = "admin";
-   public static final String adminPassword = "admin0040@";
+   public static final String url = "http://magento2.demo.ubertheme.com/";
+   public static final String customerUsername = "roni_cost@example.com";
+   public static final String customerPassword = "roni_cost@example.com";
+   public static final String adminUsername = "megamenu";
+   public static final String adminPassword = "megamenu@123";
 
 
 }
